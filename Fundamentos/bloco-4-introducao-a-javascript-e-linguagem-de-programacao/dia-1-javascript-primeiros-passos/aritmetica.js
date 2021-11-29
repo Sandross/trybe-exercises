60546984dd2b0b9fa3c4718dca6741dc7fa1ac46
Sandro Bistene <sandrosalasfilho3@gmail.com>
2021-11-29 14:28:58 -0500

    primeiro exercício

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-linguagem-de-programacao/dia-1-javascript-primeiros-passos/aritmetica.js
@@ -0,0 +1,19 @@
+const a = 4;
+const b = 10;
+
+adição = (a+b);
+console.log('adição=' + adição);
+subtração = (a-b);
+console.log('subtração=' + subtração);
+multiplicação = (a*b);
+console.log('multiplicação=' + multiplicação);
+divisão = (a/b);
+console.log('divisão=' + divisão);
+módulo = (a%b);
+console.log('módulo=' + módulo);
+if(a>b){
+    console.log(a+' '+'É maior.')
+}
+else{
+    console.log(b +' '+'É maior.')
+}
