615c9df91e91575553cf22c0d6aa3404899ca833
Sandro Bistene <sandrosalasfilho3@gmail.com>
2022-02-17 00:20:45 -0300

    Exercicios do dia

---
+++ Front-End/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicios/exercicio-2/exercicio2/src/App.js
@@ -0,0 +1,14 @@
+import Header from "./Header.jsx";
+import Content from "./Content.jsx";
+import Footer from "./Footer";
+function App() {
+  return (
+    <>
+    <Header/>
+    <Content/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
